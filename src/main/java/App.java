34ef26513d7bb7134f2ba3ffaca20068fee8b386
Saikrishna95 <sai.krishna@niki.ai>
2020-09-15 11:28:43 +0530

    added shade plugin for mvn package and spotless plugin for code formatting and java jar run command

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,10 @@
 public class App {
 
-    public static void main(String[] args){
-        System.out.println("Server started");
-    }
+  // $ mvn clean package
+  // $ java -jar target/LogManagement-1.0.jar
+  public static void main(String[] args) {
+    System.out.println("Server started");
+
+    System.out.println("Server Terminated");
+  }
 }
